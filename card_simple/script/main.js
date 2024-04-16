3a9941e42b1f6b31605126240410a90b19f97a19
gregybrs <134396879+gregybrs@users.noreply.github.com>
2024-04-17 00:01:04 +0200

    Layout

--- card_simple/script/main.js
+++ card_simple/script/main.js
@@ -1,36 +1,33 @@
 $(document).ready(function () {
-    const wrapper = $('wrapper');
+    const wrapper = $('#wrapper');
     rocketData.forEach(rocket => {
-        const card =
-            $(`<div class="card">
-<div class="card-content">
-    <div class="card-number">C1</div>
-    <div class="card-title">
-        <div class="card-name">Voyager1</div>
-        <div class="card-group">Probes</div>
-    </div>
+        const card = $(`<div class="card">
+       
+            <div class="card-content">
+            <div class="rocket-image" style="background-image: url('${rocket.image}');">
+            <div class="card-title">${rocket.name}</div>
+        </div>
+                
+                    
 
-    <div class="rocket-image">
-        <img src="images/voyager.webp" alt="Voyager1">
-    </div>
-    <div class="goal-icon">Goal</div>
-    <div class="rocket-info">Solar system exploration</div>
+               
+                <div class="goal-icon">${rocket.mission_goal}</div>
+                <div class="rocket-info">${rocket.group} exploration</div>
 
-    <div class="stat-icon">mission duration</div>
-    <div class="stat-value">19000</div>
-    <div class="stat-icon">max speed</div>
-    <div class="stat-value">62000</div>
-    <div class="stat-icon">max earth distance</div>
-    <div class="stat-value">22286000</div>
-    <div class="stat-icon">development cost</div>
-    <div class="stat-value">250</div>
-    <div class="stat-icon">weight</div>
-    <div class="stat-value">773</div>
-    <div class="stat-icon">year build</div>
-    <div class="stat-value">1977</div>
-
-</div>
-</div>`);
+                <div class="stat-icon">mission duration</div>
+                <div class="stat-value">${rocket.mission_duration}</div>
+                <div class="stat-icon">max speed</div>
+                <div class="stat-value">${rocket.max_speed}</div>
+                <div class="stat-icon">max earth distance</div>
+                <div class="stat-value">${rocket.max_earth_distance}</div>
+                <div class="stat-icon">development cost</div>
+                <div class="stat-value">${rocket.development_cost}</div>
+                <div class="stat-icon">weight</div>
+                <div class="stat-value">${rocket.weight}</div>
+                <div class="stat-icon">year build</div>
+                <div class="stat-value">${rocket.year_built}</div>
+            </div>
+        </div>`);
         wrapper.append(card);
     });
 });
